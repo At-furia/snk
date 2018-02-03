715702ae9709119119984cf21f569f0ee574376f
At-furia <alexandre.1608@hotmail.fr>
2018-02-03 02:25:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var prefix = ("<");
 var randnum = 0;
 
 bot.on('ready', () => {
-    bot.user.setPresence({ game: { name: 'SNK - <help', type: 2}})
+    bot.user.setPresence({ game: { name: 'SNK - <help', type: 4}})
     console.log("Bot Ready !");
 
 });
