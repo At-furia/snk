c39998098e483bf70c920cfe9615b91294192866
At-furia <alexandre.1608@hotmail.fr>
2018-02-08 21:31:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -294,19 +294,18 @@ bot.on('message', message => {
   //          .write();
   //      break;
 //*
- case "stats" : 
+// case "stats" : 
 
-            var xp = db.get("xp").filter({username: msgauthor}).find('xp').value()
-        var ptc = db.get("ptc").filter({username: msgauthor}).find('ptc').value()
-        var ptckill = db.get("ptckill").filter({username: msgauthor}).find('ptckill').value()
-        var xpfinal = Object.values(xp);
-        var ptcfinal = Object.values(ptc);
-        var ptckillfinal = Object.values(ptckill);
-        var xp_embed = new Discord.RichEmbed()
-            .addField("Messages :", `${message.author.username} : ${xpfinal[1]} messages postés` )
-            .addField("Minijeux :", `Chasse de Titans : ${ptcfinal[1]} points
-Apocalypse Titans : ${ptckillfinal[1]} Titans tués ` )
-        message.channel.send({embed: xp_embed});
+//            var xp = db.get("xp").filter({username: msgauthor}).find('xp').value()
+//        var ptc = db.get("ptc").filter({username: msgauthor}).find('ptc').value()
+ //       var ptckill = db.get("ptckill").filter({username: msgauthor}).find('ptckill').value()
+ //       var ptcfinal = Object.values(ptc);
+ //       var ptckillfinal = Object.values(ptckill);
+ //       var xp_embed = new Discord.RichEmbed()
+ //           .addField("Messages :", `${message.author.username} : ${xpfinal[1]} messages postés` )
+ //           .addField("Minijeux :", `Chasse de Titans : ${ptcfinal[1]} points
+//Apocalypse Titans : ${ptckillfinal[1]} Titans tués ` )
+ //       message.channel.send({embed: xp_embed});
 
     }
     
