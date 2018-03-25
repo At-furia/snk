f19c4cdb52a2dd9290c7ee132c4ddd8ee1e71362
At-furia <alexandre.1608@hotmail.fr>
2018-03-26 00:40:49 +0200

    Add files via upload

---
+++ commands/ban.js
@@ -0,0 +1,26 @@
+function ban(message,prefix,bot){
+
+    if(message.content.startsWith(prefix +'ban')){
+   
+        if (!message.channel.permissionsFor(message.member).hasPermission("BAN_MEMBERS")){
+            message.reply("Tu n'as pas le droit de ban ! ;)")
+        }else{
+            var memberban = message.mentions.users.first();
+            console.log(memberban)
+            console.log(message.guild.member(memberban).bannable)
+            if(!memberban){
+                message.reply("L'utilisateur n'existe pas !");
+            }else{
+                if(!message.guild.member(memberban).bannable){
+                    message.reply("Utilisateur impossible a ban !");
+                }else{
+                    message.guild.member(memberban).ban().then((member) => {
+                    message.channel.send(`${member.displayName}a été ban du serveur !`);
+                }).catch(() => {
+                    message.channel.send("Ban Refusé!")
+                })
+            }
+        }}
+    }}
+
+    module.exports = ban
---
+++ commands/help.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+function help(message,prefix){
+    if(message.content.startsWith(prefix + 'help')){
+        
+        var help_embed = new Discord.RichEmbed()
+            .setColor('#D9F200')
+            .addField("Fonctionnement des commandes", "Chaque membre possède les commande de son grade sur le discord ainsi que les commandes des grades inférieurs ")
+            .addField("Commandes Brigade d'entrainement", "<réseaux Affiche les différents réseaux sociaux de la communauté SNK - FR\n<b-spéciales Rejoindre Les Brigades spéciales\n<garnison Rejoindre La Garnison\n<bataillon Rejoindre Le Bataillon d'Exploration")
+            .addField("Commandes Bataillon d'exploration, Garnison et Brigades Spéciales ", "<chasse Pour lancer le minijeu 'Chasse' (Vos stats ne sont pas enregistrées)\n<kill Pour lancer le minijeu 'Apocalypse Titans' (Vos stats ne sont pas enregistrées) ")
+            .addField("Commandes Esquade Livaï", "<admin Affiche les commandes Admin.")
+            .setFooter("Crée par Alex_ et Eren Jäger")
+        message.channel.sendEmbed(help_embed);
+        }
+    }
+module.exports = help
