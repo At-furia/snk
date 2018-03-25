6bba8dcce6a8cedb0a807807b50df436dbe43cc1
At-furia <alexandre.1608@hotmail.fr>
2018-03-26 00:38:23 +0200

    Add files via upload

---
+++ kick.js
@@ -0,0 +1,26 @@
+function kick(message,prefix,bot){
+
+    if(message.content.startsWith(prefix +'kick')){
+
+if (!message.channel.permissionsFor(message.member).hasPermission("KICK_MEMBERS")){
+    message.reply("Tu n'as pas le droit de kick ! ;)")
+}else{
+    var memberkick = message.mentions.users.first();
+    console.log(memberkick)
+    console.log(message.guild.member(memberkick).kickable)
+    if(!memberkick){
+        message.reply("L'utilisateur n'existe pas !");
+    }else{
+        if(!message.guild.member(memberkick).kickable){
+            message.reply("Utilisateur impossible a kick !");
+        }else{
+            message.guild.member(memberkick).kick().then((member) => {
+            message.channel.send(`${member.displayName}a été kick du serveur !`);
+        }).catch(() => {
+            message.channel.send("Kick Refusé!")
+            })
+        }
+    }}
+}}
+
+module.exports = kick
