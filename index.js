6d5a671153d8fd763a3847c781f0da9093c2abca
At-furia <alexandre.1608@hotmail.fr>
2018-04-07 02:15:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,10 @@
         -La Garnison
         -Le Bataillon d'Exploration
         -Les Brigades Spéciales
+
+        *Cependant si vous préférez rejoindre le camps des titans, vous pouvez integrez également
         -Titan Shifter
+
         SNK-FR vous expliquera le fonctionnement plus bas dans vos messages :ok_hand:
         Soyez poli et courtois, un français correct est demandé au minimum.
         Le respect est de vigueur, les propos rascistes, injure ou autre ne seront pas toléré.`)
@@ -68,10 +71,15 @@
         var bienvenue2_embed = new Discord.RichEmbed()
         .addField(`Présentation de SNK-FR`,`
         Bonjour je me présente : SNK-FR, je suis votre "guide" dans cette ville, je ne peux malheuresement pas vous répondre directement en message privé, il faudra donc tout me dire sur un des canaux de discussion !
-        Nous allons commencer par choisir votre corps d'armée !
-        Pour cela il vous suffit de taper <garnison OU <bataillon OU <b-spéciales OU <shifter
-        
-        Une fois votre corps d'armée choisit, vous avez a disposition plusieurs commandes
+        Pour cela deux choix s'offre a vous :
+
+        -Rejoindre l'armée humaine: 
+        Pour cela il vous suffit de taper <garnison OU <bataillon OU <b-spéciales 
+
+        -Rejoindre le camps des titans:
+        Pour cela il vous suffit de taper  <shifter  
+
+        Une fois votre camp choisit, vous avez a disposition plusieurs commandes
         qui vous seront détaillées en tapant <help !
         Il vous sera aussi possible de gagner des titres grâce a des "jeux" !`)
         member.sendMessage(bienvenue2_embed);
