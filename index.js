c80dfff2e5ffd8f8c49ff56531b36f53a604ad06
At-furia <alexandre.1608@hotmail.fr>
2018-04-12 20:38:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -301,7 +301,7 @@ var party_launch = false;
 bot.on('message', function(message){
 
     if(message.content == prefix + "chasse"){
-        if(!message.member.roles.some(r=>["Escouade Livaï","La Garnison","Les Brigades Spéciales","Le Bataillon d'Exploration"].includes(r.name)) )
+        if(!message.member.roles.some(r=>["Titan Shifter","Escouade Livaï","La Garnison","Les Brigades Spéciales","Le Bataillon d'Exploration"].includes(r.name)) )
         return message.reply("Vous n'êtes pas assez gradé pour utiliser cette commande !");
         
         message.reply("Chasse lancée ! :telescope: Je vois des Titans au loin, essaye de les compter ! tu as juste me dire combien tu vois et je te dirais si j'en vois autant ou pas.. D'après moi il y'a entre 0 et 500 Titans !  ")
