52d862921d8ad791c3ce507dfb804776ef94c00d
At-furia <alexandre.1608@hotmail.fr>
2018-04-12 16:58:33 +0200

    Add files via upload

---
+++ commands/rdm.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js');
+const low = require('lowdb')
+const FileSync = require ('lowdb/adapters/FileSync')
+const fs = require("fs");
+
+const persoadapter = new FileSync('perso.json');
+const luladapter = new FileSync('lul.json');
+const bonusadapter = new FileSync('bonus.json');
+const persodb = low(persoadapter);
+const luldb = low(luladapter);
+const bonusdb = low(bonusadapter);
+
+var rperso = persodb.get('perso').size().value();
+var rlul = luldb.get('lul').size().value();
+var rbonus = bonusdb.get('bonus').size().value();
+
+function rdm(message,prefix,bot){
+
+    if (message.content.startsWith(prefix + 'action')) {
+
+randomperso();
+var lul = luldb.get(`lul[${randnum}].lul_value`).toString().value();
+console.log(randnum);
+
+
+randomlul();
+var perso = persodb.get(`perso[${randnum}].perso_value`).toString().value();
+console.log(randnum);
+
+randombonus();
+var bonus = bonusdb.get(`bonus[${randnum}].bonus_value`).toString().value();
+console.log(randnum);
+
+var rdm_embed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .addField("Vous vous promenez tranquillement et tout d'un coup vous apercevez quelqu'un..", `${perso}` + ' est en train de' + ` ${lul},` + ` ${bonus}`)
+message.channel.sendEmbed(rdm_embed)
+    }}
+
+    function randomperso(min, max) {
+        min = Math.ceil(0);
+        max = Math.floor(rperso);
+        randnum = Math.floor(Math.random() * (max - min) + min);
+    
+    }
+    function randomlul(min, max) {
+        min = Math.ceil(0);
+        max = Math.floor(rlul);
+        randnum = Math.floor(Math.random() * (max - min) + min);
+    
+    }
+    function randombonus(min, max) {
+        min = Math.ceil(0);
+        max = Math.floor(rbonus);
+        randnum = Math.floor(Math.random() * (max - min) + min);
+    
+    }
+module.exports = rdm
