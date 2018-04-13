f5ea977c74ea05f9b60ebb0aaeee67d802f77231
At-furia <alexandre.1608@hotmail.fr>
2018-04-13 02:56:11 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,13 +5,13 @@ function help(message,prefix){
         
         var help_embed = new Discord.RichEmbed()
                 .setColor('#D9F200')
-                .addField("Fonctionnement des commandes", "Chaque membre possède les commande de son grade sur le discord ainsi que les commandes des grades inférieurs ")
-                .addField("Commandes Brigade d'entrainement", "<réseaux Affiche les différents réseaux sociaux de la communauté SNK - FR")
-                .addField("Commandes Bataillon d'exploration, Garnison et Brigades Spéciales ", "<sugg Envoyer une suggestion d'amélioration du serveur Discord.\n<stats Voir son nombre de messages sur le serveur. ")
-                .addField("Commandes Esquade Livaï", "<admin Affiche les commandes Admin.")
-                .setFooter("Crée par Alex_ et Eren Jäger")
-            message.channel.sendEmbed(help_embed);
-            console.log("Commande Help demandée"); 
+            .addField("Fonctionnement des commandes", "Chaque membre possède les commande de son grade sur le discord ainsi que les commandes des grades inférieurs ")
+            .addField("Commandes Brigade d'entrainement", "<réseaux Affiche les différents réseaux sociaux de la communauté SNK - FR\n<b-spéciales Rejoindre Les Brigades spéciales\n<garnison Rejoindre La Garnison\n<bataillon Rejoindre Le Bataillon d'Exploration\n<shifter Rejoindre les Titans Shifter")
+            .addField("Commandes Bataillon d'exploration, Garnison et Brigades Spéciales ", "<chasse Pour lancer le minijeu 'Chasse'\n<kill Pour lancer le minijeu 'Apocalypse Titans' ")
+            .addField("Commandes Titan Shifter ", "<manger Pour lancer le minijeu 'Apocalypse Titans'\n<chasse Pour lancer le minijeu 'Chasse' ")
+            .addField("Commandes Escouade Livaï", "<admin Affiche les commandes Admin.")
+            .setFooter("Crée par Alex_ et Eren Jäger")
+        message.channel.sendEmbed(help_embed);
         }
     }
 module.exports = help
