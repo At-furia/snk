753f80df66fd6f5051d9364157557e730e7c3f43
At-furia <alexandre.1608@hotmail.fr>
2018-05-02 01:44:41 +0200

    Update réseaux.js

--- commands/réseaux.js
+++ commands/réseaux.js
@@ -12,8 +12,7 @@ function réseaux(message,prefix){
             .addField("Facebook", "https://www.facebook.com/erenjager.snkfrance.9")
             .addField("Youtube", " Chaine Youtube de SNK-FR : https://www.youtube.com/channel/UCKzU9176ms-0z6Kmjpz2Onw")
             .addField("Twitch", " SNK-FR : https://www.twitch.tv/snkerenjager78/")
-            .addField("Psn", "Eren-Jage_SNKFR")
-            //.addfield("Snapchat", "erenjagersnkfr")
+            .addField("Autre", "Psn : Eren-Jage_SNKFR\nSnapchat : erenjagersnkfr\nInstagram: https://www.instagram.com/snkfra/")
             .setFooter("Crée par Alex_ et Eren Jäger","https://vignette.wikia.nocookie.net/shingekinokyojin/images/2/2e/Eren_anime_character_image.png")
         message.channel.sendEmbed(réseaux_embed);
         console.log("Commande réseaux demandée"); 
