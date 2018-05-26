62dbde736c8bbb40233701ba1648a08a5b65e9d9
At-furia <alexandre.1608@hotmail.fr>
2018-05-26 17:54:46 +0200

    Create spoil.js

---
+++ commands/spoil.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const low = require('lowdb')
+const FileSync = require ('lowdb/adapters/FileSync')
+const fs = require("fs");
+
+function spoil(message,prefix,bot){
+
+if (message.author.bot) return;
+        if (message.channel.type === 'dm') return;
+    
+    let guild = message.member.guild;
+    let Role = guild.roles.find('name', 'SPOIL');
+    let removespoil = guild.roles.find('name', "SPOIL");
+    
+    if(!message.content.startsWith(prefix)) return;
+    
+    if (message.content.startsWith(prefix + 'spoil')) {
+        
+        
+        if (message.member.roles.has(Role.id)) {
+            message.channel.sendMessage('Canal Spoil désactivé !');
+            message.member.removeRole(removespoil);
+
+    }
+        else {
+        message.member.addRole(Role);
+        message.channel.sendMessage('Canal Spoil activé !');
+}}}
+
+module.exports = spoil
