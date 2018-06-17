e9ad2527788ee83419e51247758b135ccb0e64ca
At-furia <alexandre.1608@hotmail.fr>
2018-06-17 18:46:16 +0200

    Update trois.js

--- commands/trois.js
+++ commands/trois.js
@@ -11,16 +11,22 @@ function trois(message,prefix,bot){
         random = Math.ceil(Math.random() * 26);
         random1 = Math.ceil(Math.random() * 26);
         random2 = Math.ceil(Math.random() * 26);
-
+        
         var lettre_aleatoire = alphabet[random];
         var lettre_aleatoire1 = alphabet[random1];
         var lettre_aleatoire2 = alphabet[random2];
+        
+        var theme = ['','SNK','Animaux','Objets','+18','Nature','Corps','Aliments','Fruits','Légumes','Animé/Manga','Vulgaire'];    
+        randomt = Math.ceil(Math.random() * 11);
+
+        var theme_aleatoire = alphabet[randomt];
 
         var bruh_embed = new Discord.RichEmbed()
 
         .setTitle("Jeu des 3 lettres")
         .addField("Qu'est ce que le jeu des 3 lettres ?","C'est un jeu qui vous tire aléatoirement 3 lettres, avec ces lettres vous devez faire une sorte de slogan, par exemple :\nLettres aléatoire : A T V --> Armée de Titans Vegan")
         .addField("Lettres : ",` ${lettre_aleatoire}\n${lettre_aleatoire1}\n${lettre_aleatoire2} `)
+        .addField("Thème (optionnel) : ",`${theme_aleatoire}`)
         .setFooter("Codé et idée par ▼𝓐𝓵𝒆𝔁_ [Admin Bot]▲")
 
         message.channel.send({embed: bruh_embed});
