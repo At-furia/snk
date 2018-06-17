e60a2c0ab7ea477e7985cabd3a43e4b019c80f73
At-furia <alexandre.1608@hotmail.fr>
2018-06-17 15:08:34 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ function help(message,prefix){
         var help_embed = new Discord.RichEmbed()
                 .setColor('#D9F200')
             .addField("Fonctionnement des commandes", "Chaque membre possède les commande de son grade sur le discord ainsi que les commandes des grades inférieurs ")
-            .addField("Commandes Brigade d'entrainement", "<réseaux Affiche les différents réseaux sociaux de la communauté SNK - FR\n<b-spéciales Rejoindre Les Brigades spéciales\n<garnison Rejoindre La Garnison\n<bataillon Rejoindre Le Bataillon d'Exploration\n<shifter Rejoindre les Titans Shifter\n<spoil activer ou désactiver le canal Spoil")
+            .addField("Commandes Brigade d'entrainement", "<réseaux Affiche les différents réseaux sociaux de la communauté SNK - FR\n<b-spéciales Rejoindre Les Brigades spéciales\n<garnison Rejoindre La Garnison\n<bataillon Rejoindre Le Bataillon d'Exploration\n<shifter Rejoindre les Titans Shifter\n<spoil activer ou désactiver le canal Spoil\n<3l Jeu des 3 lettres")
             .addField("Commandes Bataillon d'exploration, Garnison et Brigades Spéciales ", "<chasse Pour lancer le minijeu 'Chasse'\n<kill Pour lancer le minijeu 'Apocalypse Titans' ")
             .addField("Commandes Titan Shifter ", "<manger Pour lancer le minijeu 'Apocalypse Titans'\n<chasse Pour lancer le minijeu 'Chasse' ")
             .addField("Commandes Escouade Livaï", "<admin Affiche les commandes Admin.")
