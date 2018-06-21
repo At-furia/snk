131ffa6713d2619297867c2e5af9481957046b80
At-furia <alexandre.1608@hotmail.fr>
2018-06-21 04:08:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@
     bot.on('ready', () => {
         bot.user.setPresence({ game: { name: 'SNK - <help', type: 3}})
         bot.user.setStatus("idle")
-    //    bot.channels.get("406139512985747466").send(`Bot redémarré`);
+        bot.channels.get("406139512985747466").send(`Bot redémarré`);
 
 
     });
