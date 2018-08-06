42baecc8c7a220bfd1eb411472872e5d803d7f53
At-furia <alexandre.1608@hotmail.fr>
2018-08-06 13:17:34 +0200

    Add files via upload

---
+++ commands/boss.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js');
+const low = require('lowdb')
+const FileSync = require ('lowdb/adapters/FileSync')
+const fs = require("fs");
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({ xp: [], sugg: [], story: [], blagues:[], ptc:[], pt:[], ptckill:[], emojimort:[], titre:[], inventaireplayer:[], argent:[], argentplayer:[], shop:[], inventaire:[], banque: [], localisation:[], boss:[]}).write()
+
+function boss(message,prefix){
+    db.defaults({ xp: [], sugg: [], story: [], blagues:[], ptc:[], pt:[], ptckill:[], emojimort:[], titre:[], inventaireplayer:[], argent:[], argentplayer:[], shop:[], inventaire:[], banque: [], localisation:[], boss:[]}).write()
+
+    var joueur = message.author.username;
+    if (message.author.bot) return;
+    var viedb = db.get("boss").filter({ boss: "titan" }).find('vie').value();
+    var vie = Object.values(viedb)
+
+    if (message.content === prefix + "bs") {
+        titanalive = true;
+        message.reply("boss lancé")
+        var timeout = setTimeout(function () {
+
+            titanalive = false;
+            message.channel.send("")
+
+                .catch(console.error); // add error handling here
+        }, 1 * 43200000);
+    }
+    var vielolmdr = Math.ceil(Math.random() * 50);
+    
+    if (message.content === prefix + "f") {
+            if (titanalive && message.content != null) {
+                if (vie[1] >= 150)
+                db.get("boss").find({ boss: "titan" }).assign({ vie: vie[1] -= vielolmdr }).write();
+                message.reply(`Attaque le titan et lui fait perdre ${vielolmdr} points de vie ! [${vie[1]}/15000]`)
+            }
+            if (vie[1] <= 150 && vie[2] <= 1) {
+                message.reply("Le titan se met a hurler et puis s'écroule au sol, vous l'avez vaincu !")
+                db.get("boss").find({ boss: "titan" }).assign({ alive: vie[2] = 2 }).write();
+                titanalive = false
+            }
+        
+    }
+    }
+module.exports = boss
