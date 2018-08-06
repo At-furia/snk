38378a93612c6888d5211206e0ab217d4b1344ba
At-furia <alexandre.1608@hotmail.fr>
2018-08-06 13:41:05 +0200

    Update boss.js

--- commands/boss.js
+++ commands/boss.js
@@ -18,7 +18,7 @@ function boss(message,prefix){
 
     if (message.content === prefix + "bs") {
         titanalive = true;
-        message.reply("boss lancé")
+        message.reply("Un titan géant arrive près des humains, vous décidez d'unir vos forces pour essayer de le tuer ('<f' Pour combattre le Titan)")
         var timeout = setTimeout(function () {
 
             titanalive = false;
@@ -29,6 +29,9 @@ function boss(message,prefix){
     }
     var vielolmdr = Math.ceil(Math.random() * 50);
     
+    let chs = message.guild.channels.find("name", "titans");
+
+    if (message.channel === chs ) {
     if (message.content === prefix + "f") {
             if (titanalive && message.content != null) {
                 if (vie[1] >= 150)
@@ -39,6 +42,41 @@ function boss(message,prefix){
                 message.reply("Le titan se met a hurler et puis s'écroule au sol, vous l'avez vaincu !")
                 db.get("boss").find({ boss: "titan" }).assign({ alive: vie[2] = 2 }).write();
                 titanalive = false
+            }
+    }
+    }
+    
+    
+        var vieedb = db.get("boss").filter({ boss: "humains" }).find('vie').value();
+    var viee = Object.values(vieedb)
+    
+    if (message.content === prefix + "bh") {
+        titanalivee = true;
+        message.reply("Vous décider d'attaquer les humains, battez vous et tuez les tous ! ('<f' pour attaquer les humains)")
+        var timeout = setTimeout(function () {
+
+            titanalivee = false;
+            message.channel.send("")
+
+                .catch(console.error); // add error handling here
+        }, 1 * 43200000);
+    }
+    var vielolmddr = Math.ceil(Math.random() * 50);
+    
+    
+    let chds = message.guild.channels.find("name", "humains");
+
+    if (message.channel === chds ) {
+    if (message.content === prefix + "f") {
+            if (titanalivee && message.content != null) {
+                if (viee[1] >= 150)
+                db.get("boss").find({ boss: "humains" }).assign({ vie: viee[1] -= vielolmddr }).write();
+                message.reply(`Attaque le titan et lui fait perdre ${vielolmddr} points de vie ! [${viee[1]}/20000]`)
+            }
+            if (viee[1] <= 150 && viee[2] <= 1) {
+                message.reply("Tout les humains présents ont été tués !")
+                db.get("boss").find({ boss: "humains" }).assign({ alive: viee[2] = 2 }).write();
+                titanalivee = false
             }
         
     }
