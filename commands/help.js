b07ee503f151562ba5771f93e18c9bb89cd111d7
At-furia <alexandre.1608@hotmail.fr>
2018-08-20 10:44:14 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ function help(message,prefix){
         var help_embed = new Discord.RichEmbed()
                 .setColor('#D9F200')
             .addField("Fonctionnement des commandes", "Chaque membre possède les commande de son grade sur le discord ainsi que les commandes des grades inférieurs ")
-            .addField("Nouvelles commandes !","<fac Choisis une faction aléatoire si vous avez le rôle Brigade d'Entrainement"
+            .addField("Nouvelles commandes !","<fac Choisis une faction aléatoire si vous avez le rôle Brigade d'Entrainement")
             .addField("Autres commandes !", "<signalement Ouvre le panneau de signalement SNK\n<love +@pseudo Affiche votre % d'amour entre vous et le membre mentionné\n<haine +@pseudo Comme love mais version haine.")
             .addField("Commandes Brigade d'entrainement", "<réseaux Affiche les différents réseaux sociaux de la communauté SNK - FR\n<spoil activer ou désactiver le canal Spoil et Manga-scans\n<3l Jeu des 3 lettres")
             .addField("Commandes Bataillon d'exploration, Garnison et Brigades Spéciales ", "<chasse Pour lancer le minijeu 'Chasse'\n<kill Pour lancer le minijeu 'Apocalypse Titans' ")
