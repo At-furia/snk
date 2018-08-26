4fa39e6809c09f8a889d33086846a0e046371b85
At-furia <alexandre.1608@hotmail.fr>
2018-08-26 21:09:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -418,15 +418,12 @@ bot.on('message', function (message) {
             let ttt = message.guild.channels.find("name", "mini-jeux-🎲");
           if (message.channel === ttt) {     
                    if(!message.member.roles.some(r=>["Escouade Livaï","Animateur"].includes(r.name)) ) 
+                       return message.delete();
+
 return bot.channels.get("483094336259489812").send(message.content.slice(0, message.content.length) + " " + userVar); 
+
           }
              
-                       if (message.channel === ttt) {     
-
-                if(!message.member.roles.some(r=>["Escouade Livaï","Animateur"].includes(r.name)) ) 
-             return message.delete();
-              
-          }
             }
         )
 
