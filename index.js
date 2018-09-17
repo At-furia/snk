6ecf1cb6294cd0dea03739045a291dc3129bcf6c
At-furia <alexandre.1608@hotmail.fr>
2018-09-17 21:55:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
     const FileSync = require ('lowdb/adapters/FileSync')
     const fs = require("fs");
     const config = require("./config.json");
-    require('events').EventEmitter.prototype._maxListeners = 300;
+    require('events').EventEmitter.prototype._maxListeners = 301;
 
     const adapter = new FileSync('database.json');
     const db = low(adapter);
