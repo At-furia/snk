7eebefb23f2af64bdbf15f9148f9c38ceeb6fb08
At-furia <alexandre.1608@hotmail.fr>
2018-10-09 11:10:05 +0200

    Update love.js

--- commands/love.js
+++ commands/love.js
@@ -5,30 +5,63 @@ const fs = require("fs");
 
 function love(message,prefix,bot){
 
-    if(message.content.startsWith(prefix +'love')){
+     if(message.content.startsWith(prefix +'love')){
+            if (message.author.bot) return;
 
-        if (message.author.bot) return;
-
-        randomlove = Math.ceil(Math.random() * 101);
-
-        var memberlove = message.mentions.users.first();
+            randomlove = Math.ceil(Math.random() * 101);
     
-        if(!memberlove){
-            message.reply("L'utilisateur n'existe pas !");
+            var memberlove = message.mentions.users.first();
         
-            }else{
+            if(!memberlove){
+                message.reply("L'utilisateur n'existe pas !");
+            
+                }
+                  
+                if (randomlove < 50) {
                 
-                    //    var love_embed = new Discord.RichEmbed()
+                var love_embed = new Discord.RichEmbed()
+           // message.reply("Il y'a a " + `${randomlove}` + "% d'amour entre toi et " + memberlove );
+           .setAuthor("💖 Love Machine 💖",message.author.avatarURL)
+           .setDescription(`💗**${memberlove.tag}**\n💗**${message.author.tag}**`)
+                .setColor(0x00AE86)
+                .addField(`${randomlove}% || ​██████████ ​ || `,"**Résultat:**`Amour imposible`")
 
-            message.reply("Il y'a a " + `${randomlove}` + "% d'amour entre toi et " + memberlove );
-                //.setAuthor("client.user.username","client.user.avatarURL")
-               // .addField("❤ Love machine ❤",`${memberlove}\nclient.user.username`)
-                //.setColor(0x00AE86)
+                            message.channel.send(love_embed)
+            }
+            if (randomlove < 80 && randomlove > 49) {
+                
+                var love_embed = new Discord.RichEmbed()
+           // message.reply("Il y'a a " + `${randomlove}` + "% d'amour entre toi et " + memberlove );
+           .setAuthor("💖 Love Machine 💖",message.author.avatarURL)
+           .setDescription(`💗**${memberlove.tag}**\n💗**${message.author.tag}**`)
+                .setColor(0x00AE86)
+                .addField(`${randomlove}% || ​███████████████████ ​ || `,"**Résultat:**`Un amour est possible entre vous`")
 
+                            message.channel.send(love_embed)
+            }
+            if (randomlove > 80 && randomlove < 101) {
+                
+                var love_embed = new Discord.RichEmbed()
+           // message.reply("Il y'a a " + `${randomlove}` + "% d'amour entre toi et " + memberlove );
+                .setAuthor("💖 Love Machine 💖",message.author.avatarURL)
+                .setDescription(`💗**${memberlove.tag}**\n💗**${message.author.tag}**`)
+                .setColor(0x00AE86)
+                .addField(`${randomlove}% || ​██████████████████████████████ ​ || `,"**Résultat:**`C'est l'amour fou entre vous`")
 
-                       //     message.channel.send(love_embed)
+                            message.channel.send(love_embed)
+            }
+            if (randomlove > 100 ) {
+                
+                var love_embed = new Discord.RichEmbed()
+           // message.reply("Il y'a a " + `${randomlove}` + "% d'amour entre toi et " + memberlove );
+           .setAuthor("💖 Love Machine 💖",message.author.avatarURL)
+           .setDescription(`💗**${memberlove.tag}**\n💗**${message.author.tag}**`)
+                .setColor(0x00AE86)
+                .addField(`${randomlove}% || ​████████████████████████████████████████​ || `,"**Résultat:**`Vous êtes fais pour être ensemble jusqu'à a la fin de votre vie !`")
 
-            }}
+                            message.channel.send(love_embed)
+            }
+        }
 
              var joueur = message.author.username;
             if (message.author.bot) return;
