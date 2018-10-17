fde7618a6e1b895e13e29f74f202758317a12e29
At-furia <alexandre.1608@hotmail.fr>
2018-10-18 00:21:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -643,7 +643,7 @@ message.channel.send( {files: ["./discordrpg/" + "1 (" + imageNumber + ").jpg"]}
              
 if(!db.get("xp").find({username: msgauthor}).value()){
 db.get("xp").push({username: msgauthor, xp: 1}).write();
-
+}
 if(!message.member.roles.some(r=>["Les Brigades Spéciales","Le Bataillon d'Exploration","Titan Shifter"].includes(r.name)) ){
 return bot.channels.get("500327368628633612").send(`+ 5 points pour ` + userVar + ` de la faction La Garnison et gagne ` + orbe_random + ` Orbes`)
 //var userxpdb = db.get("xp").filter({username: msgauthor}).find('xp').value();
@@ -673,11 +673,10 @@ return bot.channels.get("500327349645213706").send(`+ 5 points pour ` + userVar 
 
 }
 
-}
+
 var userorbedb = db.get("xp").filter({username: msgauthor}).find('xp').value();
 var userorbe = Object.values(userorbedb);
 console.log(`Nombre d'orbes : ${userorbe[1]}`)
-
 db.get("xp").find({username: msgauthor}).assign({username: msgauthor, xp: userorbe[1] += orbe_random}).write();
          }
      
